3ecf50e727092d870ae49c42d181d92a89836d33
Armen Ghorukhchyan <Armenghorukhchyan@gmail.com>
2021-03-22 14:00:06 +0400

    secondCommit

--- src/main.js
+++ src/main.js
@@ -13,34 +13,7 @@ new Vue({
   render: (h) => h(App),
 }).$mount("#app");
 
-// console.log("1");
-// console.log("2");
-// console.log("3");
 
-// console.log(":1");
-
-// setTimeout(() => {
-//       console.log(":2");
-// }, 2000);
-
-// console.log(":3");
-
-// console.log("-1");       
-
-// var f2 = function() {
-//   return new Promise(function(resolve) {
-//     setTimeout(function() {
-//       console.log("-2");
-//       resolve();
-//     }, 2000);
-//   });
-// };
-
-// f2().then(function() {
-//   console.log("-3");
-// });
-
-// // ("-1" , "-2" , "-3")
 
 
 
